49de1fe132dcaf512cdfa2d37e3d1f5bff82bb60
surajguduru <guduru.23bcs10012@ms.sst.scaler.com>
2024-06-26 11:33:35 +0530

    cart functionality added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,20 @@
+import { useSelector } from "react-redux";
+// import './Cart.css';
+
+function Cart() {
+    const items = useSelector((state) => state.items);
+
+    return (
+        <div className='cart'>
+            <h2>Cart</h2>
+            {Object.keys(items).map((id) => (
+                <div key={id}>
+                    <span>Product ID: {id}</span>
+                    <span> &nbsp; Quantity: {items[id].quantity}</span>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Cart;
